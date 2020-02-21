cf3e276ebf07666507d83afe4616efa7041e21fc
原嵁 <13582788510@139.com>
2020-02-21 12:06:28 +0800

    为安装\管理脚本添加Arch Linux支持
    改动只涉及到release检测相关代码，并为Arch Linux安装运行脚本所需的软件

--- v2-ui.sh
+++ v2-ui.sh
@@ -27,12 +27,16 @@ elif cat /etc/issue | grep -Eqi "ubuntu"; then
     release="ubuntu"
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
+elif cat /etc/issue | grep -Eqi "Arch Linux"; then
+    release="Arch Linux"
 elif cat /proc/version | grep -Eqi "debian"; then
     release="debian"
 elif cat /proc/version | grep -Eqi "ubuntu"; then
     release="ubuntu"
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
+elif cat /proc/version | grep -Eqi "Arch Linux"; then
+    release="Arch Linux"
 else
     echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
 fi
